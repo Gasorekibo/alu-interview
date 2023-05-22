f36da999c823e2c5f3318bd5b70914e439de3f65
Gasorekibo <m.gasore@alustudent.com>
2023-05-22 19:44:03 +0200

    minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def minOperations(n):
+    """
+        this function will return the minimun number of operation
+    """
+    if n == 1:
+        return 0
+    operations = 0
+    copy_count = 1
+    paste_count = 0
+
+    while copy_count < n:
+        if n % copy_count == 0:
+            paste_count = copy_count
+            operations += 1
+        copy_count += paste_count
+        operations += 1
+
+    return operations
+    
---
+++ minimum_operations/REDME.md
@@ -0,0 +1,1 @@
+Solutions to some python interview questions.
