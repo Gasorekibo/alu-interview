5bd14c1a8af80441698c1f484d90185777962e55
Gasorekibo <m.gasore@alustudent.com>
2023-05-22 19:58:10 +0200

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -10,15 +10,15 @@ def minOperations(n):
     """
     if n == 1:
         return 0
-    operations = 0
+    nbr_operations = 0
     copy_count = 1
     paste_count = 0
 
     while copy_count < n:
         if n % copy_count == 0:
             paste_count = copy_count
-            operations += 1
+            nbr_operations += 1
         copy_count += paste_count
-        operations += 1
+        nbr_operations += 1
 
-    return operations
+    return nbr_operations
