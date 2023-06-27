d4c8f0356e404d4a5e3d12d026fb5b932c62171e
Gasorekibo <m.gasore@alustudent.com>
2023-06-27 23:46:23 +0200

    pascals triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -8,6 +8,19 @@ def pascal_triangle(n):
     """
     Returns right angled triangle.
     """
-    if n == 0:
+    if n <= 0:
         return []
-    return [[1]] + pascal_triangle(n - 1)
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
